6266545cceaee4715b91d81486862d96f51c9c9d
Nickolay <snsarkisyan@yandex.ru>
2024-05-16 09:11:00 +0300

    init

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+import styles from "./Header.module.scss";
+import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { SelectIsAuth, logout } from "../../redux/slices/auth";
+
+export const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(SelectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm("Вы действительно хотите выйти?")) {
+      dispatch(logout());
+      window.localStorage.removeItem("token");
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>ARCHAKOV BLOG</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
